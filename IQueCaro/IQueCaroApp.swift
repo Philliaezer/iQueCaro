74dca77ecd02ae8137598ba2e869f78f4263ce2a
Anderson Duarte <reddragondggda@gmail.com>
2025-08-18 10:57:37 -0300

    chore: First commit
    Apple, you owe me my hair lost and my sanity, as long as 70% of your company as a monetary compensation for the moral damages your software has caused to me...

---
+++ IQueCaro/IQueCaroApp.swift
@@ -0,0 +1,17 @@
+//
+//  IQueCaroApp.swift
+//  IQueCaro
+//
+//  Created by Usuário Convidado on 18/08/25.
+//
+
+import SwiftUI
+
+@main
+struct IQueCaroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
